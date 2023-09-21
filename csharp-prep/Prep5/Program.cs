1eb38c5f513325053278e4ee0208f76034ff6c5c
Tebakaro Tione <tionete@LDSSCHOOLS.ORG>
2023-09-21 19:58:42 +1200

    Complete Project 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -8,23 +8,20 @@ class Program
         string userName = PromptUserName();
         int userNumber = PromptUserNumber();
         int squaredNumber = SquareNumber(userNumber);
+
         DisplayResult(userName, squaredNumber);
     }
-
     static void DisplayWelcomeMessage() => Console.WriteLine("Welcome to the Program!");
-
     static string PromptUserName()
     {
         Console.Write("Please enter your name: ");
         return Console.ReadLine();
     }
-
     static int PromptUserNumber()
     {
         Console.Write("Please enter your favorite number: ");
         return int.Parse(Console.ReadLine());
     }
-
     static int SquareNumber(int number) => number * number;
     static void DisplayResult(string name, int squaredNumber)
     {
