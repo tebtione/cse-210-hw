da4e4881ae6df93a15a9c4c480ab066a1ea2dad2
Tebakaro Tione <tionete@LDSSCHOOLS.ORG>
2023-09-28 21:07:05 +1200

    Complete class Jobs

--- prepare/Learning02/Jobs.cs
+++ prepare/Learning02/Jobs.cs
@@ -6,6 +6,5 @@ public class Job
     public string _company;
     public int _startYear;
     public int _endYear;
-
     public void Display() => Console.WriteLine($"{_jobTitle} ({_company}) {_startYear} - {_endYear}");   
 }
