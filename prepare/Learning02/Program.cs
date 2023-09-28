cdd7cadbf9be5fb7c14156147c6da7d1c410dda5
Tebakaro Tione <tionete@LDSSCHOOLS.ORG>
2023-09-28 21:08:56 +1200

    Add Program.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -15,7 +15,7 @@ class Program
         job2._company = "Apple";
         job2._startYear = 2022;
         job2._endYear = 2023;
-
+        
         Resume myResume = new Resume();
         myResume._name = "Allison Rose";
 
